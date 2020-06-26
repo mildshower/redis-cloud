1751b01c5cc610fc46f66544600a4e5e01bbd405
Sudipta Kundu <sdptknd@gmail.com>
2020-06-26 20:47:57 +0530

    added proper support for heroku

--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
-let client;
-if (process.env.REDISTOGO_URL) {
-  const rtg = require("url").parse(process.env.REDISTOGO_URL);
-  client = require("redis").createClient(rtg.port, rtg.hostname);
-  client.auth(rtg.auth.split(":")[1]);
-} else {
-  client = require("redis").createClient();
-}
+const client = require("redis").createClient(process.env.REDIS_URL);
+// if (process.env.REDISTOGO_URL) {
+//   const rtg = require("url").parse(process.env.REDISTOGO_URL);
+//   client = require("redis").createClient(rtg.port, rtg.hostname);
+//   client.auth(rtg.auth.split(":")[1]);
+// } else {
+//   client = require("redis").createClient();
+// }
 const { setValue, serveValue } = require("./handlers");
 
 const app = express();
